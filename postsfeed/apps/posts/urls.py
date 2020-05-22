3a196c027cb72a4191dcae2df354e9b0e6f38a78
funny family <maksvadik228@gmail.com>
2020-05-22 22:54:33 +0700

    added post and posts pages for each post

--- postsfeed/apps/posts/urls.py
+++ postsfeed/apps/posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import  views
 
+app_name = 'posts' # app_name - specified for django
+
 urlpatterns = [
-    path('', views.posts, name = 'posts')
+    path('', views.posts, name = 'posts'),
+    path('<int:postId>/', views.specifiedPost, name = 'specifiedPost'),
 ]
--- postsfeed/apps/posts/views.py
+++ postsfeed/apps/posts/views.py
@@ -1,9 +1,24 @@
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
-# from django.http import HttpResponse
+from .models import Post, Comment
 
 # Create your views here.
 def posts(request):
+    sortedPostsByPublicationDate = Post.objects.order_by('-post_publication_date')[:5]
     return render(
         request,
-        'posts/potsList.html'
+        'posts/potsList.html',
+        {
+            'sortedPostsByPublicationDate': sortedPostsByPublicationDate
+        }
     )
+
+def specifiedPost(request, postId):
+    try:
+        receivedPost = Post.objects.get(id = postId)
+    except:
+        raise Http404('Post not found!')
+
+    return render(request, 'posts/specifiedPost.html', {
+        'post': receivedPost
+    })

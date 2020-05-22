9c936bb7a3d287f29e939adaf4a3feb8c85e320d
funny family <maksvadik228@gmail.com>
2020-05-22 20:13:28 +0700

    added templates

--- postsfeed/apps/posts/views.py
+++ postsfeed/apps/posts/views.py
@@ -1,6 +1,9 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 # Create your views here.
 def posts(request):
-    return HttpResponse('test')
+    return render(
+        request,
+        'posts/potsList.html'
+    )
